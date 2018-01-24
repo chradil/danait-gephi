d2f3c8cce2667972505f7f40007e9ed8a691e7fa
chradil <c_hradil@cox.net>
2018-01-24 15:24:07 -0500

    Update config.js

--- gexf-js/config.js
+++ gexf-js/config.js
@@ -1,7 +1,7 @@
 /*** USE THIS FILE TO SET OPTIONS ***/
 
 GexfJS.setParams({
-    graphFile : "gexf-js-data.gexf",
+    graphFile : "gexf-js-dat.gexf",
         /*
             The GEXF file to show ! -- can be overriden by adding
             a hash to the document location, e.g. index.html#celegans.gexf

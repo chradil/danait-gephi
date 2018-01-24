695e8333f8530d618289582ecf55da2df68789ea
chradil <c_hradil@cox.net>
2018-01-24 14:06:51 -0500

    Update config.js

--- gexf-js/config.js
+++ gexf-js/config.js
@@ -25,7 +25,7 @@ GexfJS.setParams({
             False for curved edges, true for straight edges
             this setting can't be changed from the User Interface
         */
-    edgeWidthFactor : 5,
+    edgeWidthFactor : 10,
         /*
             Change this parameter for wider or narrower edges
             this setting can't be changed from the User Interface
@@ -33,7 +33,7 @@ GexfJS.setParams({
     minEdgeWidth : 5,
     maxEdgeWidth : 50,
     textDisplayThreshold: 9,
-    nodeSizeFactor : 10,
+    nodeSizeFactor : 20,
         /*
             Change this parameter for smaller or larger nodes
            this setting can't be changed from the User Interface
